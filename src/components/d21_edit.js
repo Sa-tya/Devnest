7d5ad423a32c66acf923bf837bae2d6234842cc3
Satya Prakash <80380695+Sa-tya@users.noreply.github.com>
2021-07-28 13:55:54 +0530

    Add files via upload

--- src/components/d21_edit.js
+++ src/components/d21_edit.js
@@ -5,19 +5,34 @@ import './d21_form.css';
 
 function Card(props) {
     const [edited, setEdited] = useState(false);
-    const {l_item,l_cals,setItem,setCals,delete1,edit} = props;
+    const {id, l_item, l_cals, list, setList, delete1} = props; //props destructuring
+    const [i1,setI1] = useState(l_item);
+    const [c1,setC1] = useState(l_cals);
+
+    // const Caution = l_cals>100 ? <h1>Dangerous</h1> :<div></div>;
+
+     function edit() {
+        setEdited(false)
+        setList(list.map((t) => {
+            if (t.id === id) {
+                t.cals = c1
+                t.item = i1
+            }
+            return t
+        }))
+    }
+    
     return (edited ? <div className="card">
-        <input type="text" value={l_item} onChange={(e) => setItem(e.target.value)}></input>
-        <input type="number" value={l_cals} onChange={(e) => setCals(e.target.value)}></input>
+        <input type="text" value={i1} onChange={(e) => setI1(e.target.value)}></input>
+        <input type="number" value={c1} onChange={(e) => setC1(e.target.value)}></input>
         <button className="delete" onClick={() => delete1()}>Delete</button>
-        <button className="edit" onClick={() => {
-            edit()
-            setEdited(false)
-        }}>Done</button>
+        <button className="edit" onClick={() => edit() }>Done</button>
     </div> :
         <div className="card">
-            <h1>{l_item}</h1> <h2>{l_cals}</h2>
-            <button className="delete" onClick={() => props.delete()}>Delete</button>
+            <h1> {l_item} </h1>
+            <h2> You have consumed {l_cals} calories.</h2>
+            <h3>{l_cals>100 ? "Cautions :- Unhealthy for your health." :  "" }</h3>
+            <button className="delete" onClick={() => delete1()}>Delete</button>
             <button className="edit" onClick={() => { setEdited(true) }}>Edit</button>
         </div>);
 }
@@ -30,18 +45,8 @@ function Form() {
 
     function dlt(e) {
         setList(list.filter((t) => t.id !== e))
-        console.log(e, "deleted...")
     }
 
-    function edit(id) {
-        list.map((t) => {
-            if (t.id === id) {
-                t.cals = cals
-                t.item = item
-            }
-        })
-        console.log(id, "Editted...")
-    }
     function submit(e) {
         e.preventDefault();
         let temp = ((list) => [...list, {
@@ -49,6 +54,8 @@ function Form() {
             item: item,
             cals: cals
         }])
+        setItem('')
+        setCals('')
         setList(temp)
         setId(id + 1);
     }
@@ -65,9 +72,15 @@ function Form() {
         <div className="container">
             {
                 list.map((e, i) => {
-                    // console.log(e)
-                    return <Card key={i} id={id} l_item={e.item} l_cals={e.cals} setItem={setItem} setCals={setCals}
-                        delete1={() => dlt(e.id)} edit={() => edit(e.id)} />
+                    return <Card
+                    key={i}
+                    id={e.id}
+                    l_item={e.item}
+                    l_cals={e.cals}
+                    list={list}
+                    setList={setList}
+                    delete1={() => dlt(e.id)}
+                         />
                 })
             }
         </div>
